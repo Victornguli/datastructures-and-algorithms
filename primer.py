f8d2560943a532764e374e3636f149b4d87e297e
Victor Nguli <victornjoseph@gmail.com>
2020-02-10 22:55:47 +0300

    C-1.19 solution

--- primer.py
+++ primer.py
@@ -129,3 +129,12 @@ def list_comprehension_2():
 	the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].
 	"""
 	return [i*(i+1) for i in range(0, 10)]
+
+
+def alphabet_gen():
+	"""
+	C-1.19 Demonstrate how to use Python’s list comprehension syntax to produce
+	the list [ a , b , c , ..., z ], but without having to type all 26 such
+	characters literally
+	"""
+	return [chr(i) for i in range(65, 91)]
