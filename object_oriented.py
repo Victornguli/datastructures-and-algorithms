f4ce4de3d7bbe89de31d16fb5d7228d7856609ba
Victor Nguli <victornjoseph@gmail.com>
2020-02-14 11:22:02 +0300

    Add Range class

--- object_oriented.py
+++ object_oriented.py
@@ -9,10 +9,6 @@ class Vector(object):
 		"""Creates a vector of d dimensions"""
 		self._coords = [0] * d
 
-	def get_coords(self):
-		"""Return coordinates of the vector"""
-		return self._coords
-
 	def __len__(self):
 		"""Returns the dimension of the vector"""
 		return len(self._coords)
@@ -36,7 +32,7 @@ class Vector(object):
 
 	def __eq__(self, other):
 		"""Return True if vector has same coordinates as other"""
-		return self._coords == other.get_coords()
+		return self._coords == other._coords
 
 	def __ne__(self, other):
 		"""Return True if vector does not have same coordinates as other"""
@@ -46,3 +42,56 @@ class Vector(object):
 		"""Return string representation of the vector"""
 		return '<' + str(self._coords)[1:-1] + '>'  # Adapt list representation
 
+
+class SequenceIterator:
+	"""An iterator for any of Python's sequence types"""
+
+	def __init__(self, sequence):
+		"""Create an iterator for a given sequence"""
+		self._seq = sequence  # Reference to underlying data
+		self._k = -1  # Increments to 0 first call to next
+
+	def __next__(self):
+		"""Returns the next element else raise StopIteration Error"""
+		self._k += 1  # Advance to next index
+		if self._k < len(self._seq):
+			return self._seq[self._k]  # Return the sequence item
+		else:
+			raise StopIteration()  # There are no more elements
+
+	def __iter__(self):
+		"""By convention an iterator must return itself as an iter"""
+		return self
+
+
+class Range:
+	"""A class that mimics built-in range class."""
+
+	def __init__(self, start, stop = None, step = 1):
+		"""Initializes a Range instance with semantics similar to the built-in range class"""
+		if step == 0:
+			raise ValueError('Step cannot be 0')
+
+		if stop is None:
+			start, stop = 0, start  # Should be treated as range(0, n)
+
+		# Calculate the effective length
+		self._length = max(0, (stop - start + step - 1) // 2)
+
+		# To support __getitem__ add start and step as instance variables
+		self._start = start
+		self._step = step
+
+	def __len__(self):
+		"""Return length of entries in the range"""
+		return self._length
+
+	def __getitem__(self, k):
+		"""Return entry at index k (using standard interpretation if negative)"""
+		if k < 0:
+			k += len(self)  # Attempt to convert negative indices
+
+		if not 0 <= k < self._length:
+			raise IndexError('Index out of range')
+
+		return self._start + k * self._step
