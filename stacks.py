9ef3a5edd05497c8fe667ead2032eefad530bb70
Victor Nguli <victornjoseph@gmail.com>
2020-02-24 16:11:24 +0300

    HTML tag matching using stack

--- stacks.py
+++ stacks.py
@@ -35,3 +35,42 @@ class ArrayStack:
 		return self._data.pop()
 
 
+def is_matched(expr):
+	"""
+	Code Fragment 6.4: Function for matching delimiters in an arithmetic expression.
+	Return True if all delimiters are properly match; False otherwise.
+	"""
+	lefty = '({['
+	righty = ')}]'
+	stack = ArrayStack()
+	for c in expr:
+		if c in lefty:
+			stack.append(c)
+		elif c in righty:
+			if stack.is_empty():  # If c in left then it must be in right to be a matching pair i.e () / {} / []
+				return False
+			if righty.index(c) != lefty.index(stack.pop()):  # Confirm if the index of c is same as the popped value
+				# in the stack in left side. If they don't match return False
+				return False
+	return stack.is_empty()  # Ensure that search is complete
+
+
+def is_matched_html(raw):
+	"""Returns True if all HTML tags are a proper match; False otherwise"""
+	stack = ArrayStack()
+	j = raw.find('<')  # Find first opening tag
+	while j != -1:  # Confirm if there is an HTML Tag
+		k = raw.find('>', j+1)  # Search for the closing tag
+		if k == -1:
+			return False  # Closing Tag not Found
+		tag = raw[j+1:k]  # The html tag e.g title or /title
+		if not tag.startswith('/'):
+			stack.append(tag)
+		else:
+			if stack.is_empty():
+				return False  # Empty stack yet a closing tag is found means the html tag is not valid
+			if tag[1:] != stack.pop():  # Access the tag from index 1 to remove / since its a closing tag
+				return False  # The closing tag does not match the topmost one in the stack(Should be its opening)
+		j = raw.find('<', k+1)  # j(opening tag) is now searched for from the index k+1
+	return stack.is_empty()  # If the stack is empty all tags have matches: Note how stack.pop() is used during
+	# comparison to make sure any matching tag in the stack is removed.
