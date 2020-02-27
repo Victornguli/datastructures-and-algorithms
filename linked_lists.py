59e5d48c63638ccd0641b9620ce8595eaeb03947
Victor Nguli <victornjoseph@gmail.com>
2020-02-27 22:58:24 +0300

    Queue with a circular linked list storage

--- linked_lists.py
+++ linked_lists.py
@@ -106,6 +106,66 @@ class LinkedQueue:
 		return value
 
 
+class CircularQueue:
+	"""Queue implementation using circularly linked list for storage"""
+
+	class _Node:
+		"""Non-public class for storing a Node in a linked list"""
+
+		def __init__(self, element, next):
+			"""Initialize a new node"""
+			self._element = element
+			self._next = next
+
+	def __init__(self):
+		"""Initialize a new queue"""
+		self._tail = None
+		self._size = 0
+
+	def __len__(self):
+		"""Return current size of the queue"""
+		return self._size
+
+	def is_empty(self):
+		"""Return True if queue is empty"""
+		return self._size == 0
+
+	def first(self):
+		"""Return(But do Not Remove) the first element in the queue(Queue  - Front)"""
+		if self.is_empty():
+			raise Empty('Queue is empty')
+		head = self._tail._next
+		return head._element
+
+	def dequeue(self):
+		"""Remove the first element (FIFO)"""
+		if self.is_empty():
+			raise Empty('Queue is empty')
+		head = self._tail._next
+		if self._size == 1:
+			self._tail = None
+		else:
+			self._tail._next = head._next
+		self._size -= 1
+		return head._element
+
+	def enqueue(self, e):
+		"""Add an element at the end of the queue"""
+		newest = self._Node(e, None)
+		if self.is_empty():
+			newest._next = newest
+		else:
+			newest._next = self._tail._next
+			self._tail._next = newest
+		self._tail = newest
+		self._size += 1
+
+	def rotate(self):
+		"""Rotate front element to the back of the queue"""
+		if self._size > 0:
+			self._tail = self._tail._next
+
+
 if __name__ == '__main__':
 	ls = LinkedStack()
 	for i in range(5):
