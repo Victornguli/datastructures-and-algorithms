6b3e94a2b19ebb543e752200f668788fb3b11984
Victor Joseph <victornjoseph@gmail.com>
2020-04-18 22:40:41 +0300

    Rearrange recursively

--- algorithms/recursion/recursion.py
+++ algorithms/recursion/recursion.py
@@ -185,7 +185,6 @@ def binary_sum(s, start, stop):
 # C-4.11 Describe an efficient recursive function for solving the element uniqueness problem, which runs in time that
 # is at most O(n2) in the worst case without using sorting.
 def has_duplicates(data):
-	print(data)
 	if len(data) <= 1:
 		return False
 	if data[0] == data[1]:
@@ -195,6 +194,75 @@ def has_duplicates(data):
 	if has_duplicates(data[1:]):
 		return True
 	return False
+
+
+# C-4.14 In the Towers of Hanoi puzzle, we are given a platform with three pegs, a, b, and c, sticking out of it. On
+# peg a is a stack of n disks, each larger than the next, so that the smallest is on the top and the largest is on
+# the bottom. The puzzle is to move all the disks from peg a to peg c, moving one disk at a time, so that we never
+# place a larger disk on top of a smaller one. See Figure 4.15 for an example of the case n = 4. Describe a recursive
+# algorithm for solving the Towers of Hanoi puzzle for arbitrary n. (Hint: Consider first the sub-problem of moving
+# all but the nth disk from peg a to another peg using the third as “temporary storage.”)
+def tower_hanoi(n, source, aux, target):
+	if n > 0:
+		tower_hanoi(n - 1, source, target, aux)  # Move n-1 to n1 pegs from source to auxilliary
+		if source:
+			target.append(source.pop())  # Actual movement of the peg from source to target peg
+		tower_hanoi(n - 1, aux, source, target)  # Move n-1 - n1 pegs from auxilliary to target peg
+
+
+# C-4.15 Write a recursive function that will output all the subsets of a set of n elements
+# (without repeating any subsets).
+def subsets(s):
+	if not s:
+		return [[]]
+
+	x = subsets(s[1:])
+	return x + [[s[0]] + y for y in x]
+
+
+# C-4.16 Write a short recursive Python function that takes a character string s and
+# outputs its reverse. For example, the reverse of pots&pans would be snap&stop .
+def reverse(string):
+	if len(string) == 0:
+		return string
+	else:
+		return reverse(string[1:]) + string[0]
+
+
+def remove_duplicates(s, start, end):
+	if start >= end:
+		return s
+	else:
+		s_arr = list(s)
+		s_arr[start], s_arr[end - 1] = s_arr[end - 1], s_arr[start]
+		start += 1
+		end -= 1
+		return remove_duplicates(''.join(s_arr), start, end)
+
+
+# C-4.19 Write a short recursive Python function that rearranges a sequence of integer values so that all the even
+# values appear before all the odd values.
+def rearrange_seq(seq):
+	# print(seq)
+	if len(seq) < 1:
+		return []
+	if seq[0] % 2 == 0:
+		return [seq[0]] + rearrange_seq(seq[1:])
+	else:
+		return rearrange_seq(seq[1:]) + [seq[0]]
+
+
+# C-4.20 Given an unsorted sequence, S, of integers and an integer k, describe a recursive algorithm for rearranging
+# the elements in S so that all elements less than or equal to k come before any elements larger than k. What is
+# the running time of your algorithm on a sequence of n values?
+def rearrange_2(seq, k):
+	if len(seq) < 1:
+		return []
+	if seq[0] < k:
+		print(seq)
+		return [seq[0]] + rearrange_2(seq[1:], k)
+	else:
+		return rearrange_2(seq[1:], k) + [seq[0]]
 
 
 if __name__ == '__main__':
@@ -207,8 +275,13 @@ if __name__ == '__main__':
 	# print(min_recur([1,2,3,4,5,6], 6))
 	# print(recursive_product(1000, 35))
 	# print(base_two(128))
-	# print(has_more_vowels('viictoria', 0, 0, 0))
+	print(has_more_vowels('viictoria', 0, 0, 0))
 	# draw_ruler(2, 4)
 	# print(binary_sum([1, 2, 3, 4, 5], 0, 5))
 	# print(nth_harmonic_number(5))
-	print(has_duplicates(['a', 'b', 'c', 'd']))
+	print(has_duplicates(['a', 'b', 'c', 'd', 'c']))
+	source, aux, target = [4, 3, 2, 1], [], []
+	tower_hanoi(len(source), source, aux, target)
+	# print(source, aux, target)
+	li = [1, 3, 2]
+	print(rearrange_2(li, 3))
